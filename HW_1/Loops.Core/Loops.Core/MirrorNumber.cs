dc375a97d4de65b037fe4d3f62ae010bc54abb55
Veargan <feargun@gmail.com>
2016-11-14 02:42:50 +0200

    Solve the sixth task. Add MirrorNumber & MirrorNumberTest's classes

---
+++ HW_1/Loops.Core/Loops.Core/MirrorNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops.Core
+{
+    public class MirrorNumber
+    {
+        public int FindMirrorNumber(int n)
+        {
+            int res = 0;
+
+            while (n != 0)
+            {
+                res = res*10 + n%10;
+                n /= 10;
+            }
+            res = Math.Abs(res);
+
+            return res;
+        }
+    }
+}
---
+++ HW_1/Loops.Core/Loops.NUnitTests/MirrorNumberTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Loops.Core;
+
+namespace Loops.NUnitTests
+{
+    [TestFixture]
+    public class MirrorNumberTest
+    {
+        private MirrorNumber mn;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            mn = new MirrorNumber();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            mn = null;
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue1()
+        {
+            Assert.AreEqual(1, mn.FindMirrorNumber(1));
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue2()
+        {
+            Assert.AreEqual(21, mn.FindMirrorNumber(12));
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue3()
+        {
+            Assert.AreEqual(7654321, mn.FindMirrorNumber(1234567));
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue4()
+        {
+            Assert.AreEqual(0, mn.FindMirrorNumber(0));
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue5()
+        {
+            Assert.AreEqual(1, mn.FindMirrorNumber(-1));
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue6()
+        {
+            Assert.AreEqual(21, mn.FindMirrorNumber(-12));
+        }
+
+        [Test]
+        public void FindMirrorNumberShouldReturnCorrectValue7()
+        {
+            Assert.AreEqual(7654321, mn.FindMirrorNumber(-1234567));
+        }
+    }
+}
