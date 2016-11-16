7566333e4c6059b9741c9bb31fa01146862e75b8
Veargan <feargun@gmail.com>
2016-11-16 04:11:18 +0200

    Prepare List solution

---
+++ List/AList0.Core/AList0.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AList0.Core
+{
+    public class AList0
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+
+    }
+}
---
+++ List/AList0.NUnitTests/AList0Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AList0.NUnitTests
+{
+    [TestFixture]
+    public class AList0Test
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
